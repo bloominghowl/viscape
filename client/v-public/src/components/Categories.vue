35ca1de23b3bf858ae7bf7a22f1201c25be5bb3b
Angela <kafuigalley7@gmail.com>
2024-05-21 16:09:24 +0000

    feat: removed, then  added new files. This is to streamline and personalise the project.

---
+++ client/v-public/src/components/Categories.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="categories">
+      <h1>This is a categories page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .categories {
+      min-height: 100vh;
+      display: flex;
+    }
+}
+  </style>
