0011c0b43d541228984db7d1667cff9e7cad7c0e
Angela <kafuigalley7@gmail.com>
2024-05-25 22:42:08 +0000

    feat: Removed categories.ve files, removed search bar source code from files mycloud.vue and sharedfiles.vue.

--- client/v-public/src/router/index.ts
+++ client/v-public/src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/MyCloud.vue'
-import about from '../views/Sidebar.vue'
-import Categories from '@/components/Categories.vue'
+import Sidebar from '../views/Sidebar.vue'
+import Search from '@/views/search.vue'
+import Favorites from '@/components/Favorites.vue'
 import SharedFiles from '@/components/SharedFiles.vue'
+import UnsavedFiles from '@/components/UnsavedFiles.vue'
+import UploadedFiles from '@/components/UploadedFiles.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,19 +19,33 @@ const router = createRouter({
       path: '/sidebar',
       name: 'sidebar',
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (Sidebar.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Sidebar.vue')
     },
     {
-      path: '/categories',
-      name: 'categories',
-      component: Categories
+      path: '/search',
+      name: 'search',
+      component: Search
     },
     {
       path: '/sharedfiles',
       name: 'sharedfiles',
       component: SharedFiles
+    },
+    { path: '/unsavedfiles',
+      name: 'unsavedfiles',
+      component: UnsavedFiles
+    },
+    {
+      path: '/uploadedfiles',
+      name: 'uploadedfiles',
+      component: UploadedFiles
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: Favorites
     }
   ]
 })
